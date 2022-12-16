c4c2dd2099f2a21f2ea7660caf9fe3e419a8fbc2
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-16 20:13:04 +0200

    Create backpack crud operations for movies

---
+++ app/Http/Requests/MovieRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:50',
+            'language' => 'required|min:5|max:50',
+            'release_year' => 'required|integer|min:1900|max:2050',
+            'producer_name' => 'required|min:5|max:50',
+            'genres' => 'required'
+
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
