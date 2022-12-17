ebdfda6d93518e9fd73c4a7272e1cb301221a37b
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-17 22:28:11 +0200

    Add is_admin flag to users and seed default admin to database

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (DB::table('users')->count() !== 0) {
+            return;
+        }
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin8@gmail.com',
+            'password' => bcrypt('123456admin'),
+            'is_admin' => '1'
+        ]);
+    }
+}
