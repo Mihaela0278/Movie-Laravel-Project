e8cca72d63e64dfa00fd83252cb505a99b2b95ac
Mihaela Hristova <106870022+Mihaela0278@users.noreply.github.com>
2022-12-18 17:14:36 +0200

    Add partial views and main layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="text-muted">
+
+    <div class="container">
+
+        <p class="float-right">
+            <a href="#" class="btn btn-primary scrollUp" >Back to top</a>
+        </p>
+
+        <p>&copy; 2023 </p>
+
+    </div>
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<section class="jumbotron text-center">
+
+    <div class="container">
+
+        <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+
+
+        <p class="lead text-muted">
+            Cinema is the most beautiful fraud in the world.
+            <br>The difference between life and the movies is that a script has to make sense, and life doesn't.
+        </p>
+
+    </div>
+
+</section>
